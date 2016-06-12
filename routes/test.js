58d425a4cd2f10958cb12f3ab432437cbb3892ce
xiaominge <284711103@qq.com>
2016-06-12 23:05:33 +0800

    t

--- routes/test.js
+++ routes/test.js
@@ -5,15 +5,73 @@
 var express = require('express');
 var crypto = require('crypto');
 var router = express.Router();
-var User = require('../models/test.js')
+var User = require('../models/test.js');
+var http = require('http');
+var querystring = require('querystring');
+
+
+function testGetData(test,callback){
+    http.get({host: 'www.byvoid.com'}, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (data) {
+            callback(data);
+        });
+    });
+}
+
+function testPost(callback){
+    var contents = querystring.stringify({
+        name: 'byvoid',
+        email: 'byvoid@byvoid.com',
+        address: 'Zijing 2#, Tsinghua University',
+    });
+    var options = {
+        host: 'localhost',
+        port:3000,
+        path: '/testPostServer',
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/x-www-form-urlencoded',
+            'Content-Length' : contents.length
+        }
+    };
+    var req = http.request(options, function(res) {
+        res.setEncoding('utf8');
+        res.on('data', function (data) {
+            callback(data);
+        });
+    });
+    req.write(contents);
+    req.end();
+}
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
     res.render('index', { title: 'Expresss',items:['express', 'Node.js'] ,layout:'layoutfortest'});
 });
 
+/* GET home page. */
+router.get('/testHref', function(req, res, next) {
+    res.render('testHref', { title: 'Expresss',items:['express', 'Node.js'] ,layout:'layoutfortest'});
+});
+
 router.get('/test', function(req, res, next) {
     res.render('test/testlogin', { title: '测试' ,layout:'layoutfortest'});
+});
+
+router.get('/testdata', function(req, res, next) {
+    //get
+    /*testGetData(1,function(result){
+        res.json(result);
+    });*/
+    //post
+    testPost(function(result){
+        res.json(result);
+    });
+});
+
+router.post('/testPostServer',function(req,res,next){
+    res.send('sfdsdf');
 });
 
 router.post('/test',function(req,res,next){
